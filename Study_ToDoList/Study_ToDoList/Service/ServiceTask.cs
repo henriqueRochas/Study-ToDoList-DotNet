8f52ff144a1ce6e8bea1614c3ab122e86e8f0ec2
Luiz Henrique Rocha <henrique.rochas@hotmail.com>
2026-01-09 11:26:56 -0300

    Refactored code and created new methods to organize logic

--- Study_ToDoList/Study_ToDoList/Service/ServiceTask.cs
+++ Study_ToDoList/Study_ToDoList/Service/ServiceTask.cs
@@ -1,38 +1,26 @@
-﻿using Study_ToDoList.Domain;
-
+﻿using Microsoft.Win32.SafeHandles;
+using Study_ToDoList.Domain;
+using Study_ToDoList.Repositories;
 
 namespace Study_ToDoList.Service
 {
     internal class ServiceTask
     {
 
-        public void CreateUsersTask()
+        public TaskManager CreateUsersTask()
         {
 
-            User user = new User
-            (
-               Guid.NewGuid(),
-               "Henrique",
-               "henrique@email.com.br",
-               3
-            );
-
-
-            TaskManager taskManager = new TaskManager
-            (
-                Guid.NewGuid(),
-                "Minha tarefa",
-                "Descrição qualquer",
-                TaskManager.TaskPriority.Medium
-                
-            );
-
+            User user = new User();
+            TaskManager taskManager = new TaskManager();
+            
             int amount = 2;
             string reason = "test block";
             user.CheckMaxTasks(amount);
             taskManager.Start();
             taskManager.Block(reason);
             taskManager.Complete();
+
+            return taskManager;
         }
     }
 }
